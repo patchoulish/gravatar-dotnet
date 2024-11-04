e7264c7ef777d6158c75b3e9c89aacc380adcd82
Patchouli <self@patchouli.sh>
2024-11-03 20:50:40 -0500

    Added some basic unit-tests

---
+++ source/Gravatar.Tests/GravatarServiceTests.GetProfile.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Microsoft;
+using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.TestTools;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Gravatar
+{
+	partial class GravatarServiceTests
+	{
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <returns></returns>
+		[TestCategory(
+			"GravatarService")]
+		[TestCategory(
+			"GetProfile")]
+		[TestCategory(
+			"Argument")]
+		[TestMethod]
+		public async Task GetProfileThrowOnArgumentNullTestAsync()
+		{
+			await Assert.ThrowsExceptionAsync<ArgumentNullException>(
+				() => this.gravatar.GetProfileAsync(
+					default));
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <returns></returns>
+		[TestCategory(
+			"GravatarService")]
+		[TestCategory(
+			"GetProfile")]
+		[TestCategory(
+			"Argument")]
+		[TestMethod]
+		public async Task GetProfileThrowOnArgumentEmptyTestAsync()
+		{
+			await Assert.ThrowsExceptionAsync<ArgumentException>(
+				() => this.gravatar.GetProfileAsync(
+					String.Empty));
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <returns></returns>
+		[TestCategory(
+			"GravatarService")]
+		[TestCategory(
+			"GetProfile")]
+		[TestCategory(
+			"Argument")]
+		[TestMethod]
+		public async Task GetProfileTestAsync()
+		{
+			//var profile =
+			//	await this.gravatar
+			//		.GetProfileAsync(
+			//			"05ccee223d9e152175f16538714a9cf8540f4283c009c83c6c9e39c4340ce89d");
+
+			//;
+		}
+	}
+}
---
+++ source/Gravatar.Tests/GravatarServiceTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+using Microsoft;
+using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.TestTools;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Gravatar
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	[TestClass]
+	public sealed partial class GravatarServiceTests
+	{
+		private GravatarService gravatar;
+
+		/// <summary>
+		/// 
+		/// </summary>
+		[TestInitialize]
+		public void Initialize()
+		{
+			var clientApiKey =
+				"1453:gk-AYv3DZ64ZINh4Vk9v9zKvcbJGjf1_c4Qr9RWLkEtnkjZMlqTN5GRoejqxYLYr";
+
+			this.gravatar =
+				new GravatarService(
+					clientApiKey);
+		}
+	}
+}
