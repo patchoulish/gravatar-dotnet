6147288197585b039a5a102a04b9971956bed712
Patchouli <self@patchouli.sh>
2024-11-05 10:57:35 -0500

    Add extension project files

---
+++ source/Gravatar.Extensions.DependencyInjection/GravatarServiceOptions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Gravatar
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	public sealed class GravatarServiceOptions
+	{
+		/// <summary>
+		/// 
+		/// </summary>
+		public Uri BaseUrl { get; set; } =
+			GravatarService.DefaultBaseUrl;
+
+		/// <summary>
+		/// 
+		/// </summary>
+		public string ApiKey { get; set; }
+	}
+}
---
+++ source/Gravatar.Extensions.DependencyInjection/ServiceCollectionExtensions.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+using Microsoft;
+using Microsoft.Extensions;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Gravatar
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	public static class ServiceCollectionExtensions
+	{
+		/// <summary>
+		/// 
+		/// </summary>
+		private const string GravatarHttpClientName =
+			"gravatar";
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="services"></param>
+		/// <param name="optionsCallback"></param>
+		/// <returns></returns>
+		public static IHttpClientBuilder AddGravatarHttpClient(
+			this IServiceCollection services,
+			Action<GravatarServiceOptions> optionsCallback = default)
+		{
+			ArgumentNullException
+				.ThrowIfNull(
+					services,
+					nameof(services));
+
+			var options =
+				new GravatarServiceOptions();
+
+			return services
+				.AddHttpClient<IGravatarService, GravatarService>(
+					GravatarHttpClientName,
+					(httpClient) =>
+					{
+						// Configure the base URL for the client.
+						httpClient.BaseAddress =
+							options.BaseUrl;
+
+						// NOTE: Authentication is optional.
+						if (options.ApiKey != default)
+						{
+							// Configure the default request headers for the client.
+							httpClient.DefaultRequestHeaders.Authorization =
+								new AuthenticationHeaderValue(
+									"Bearer",
+									options.ApiKey);
+						}
+					});
+		}
+	}
+}
