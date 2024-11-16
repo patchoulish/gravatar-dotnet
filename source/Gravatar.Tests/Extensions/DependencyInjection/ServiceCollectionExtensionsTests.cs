6f684a5f46618da405c79e50284d618357972ce3
Patchouli <self@patchouli.sh>
2024-11-16 18:57:23 -0500

    Add some more unit tests

---
+++ source/Gravatar.Tests/Extensions/DependencyInjection/ServiceCollectionExtensionsTests.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+using Microsoft;
+using Microsoft.Extensions;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.TestTools;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Gravatar.Extensions.DependencyInjection
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	[TestClass]
+	public sealed class ServiceCollectionExtensionsTests
+	{
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <returns></returns>
+		[TestCategory(
+			"ServiceCollectionExtensions")]
+		[TestCategory(
+			"AddGravatarHttpClient")]
+		[TestCategory(
+			"Argument")]
+		[TestMethod]
+		public void AddGravatarHttpClientThrowOnArgumentNullTest()
+		{
+			Assert.ThrowsException<ArgumentNullException>(
+				() => ServiceCollectionExtensions
+					.AddGravatarHttpClient(
+						default));
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <returns></returns>
+		[TestCategory(
+			"ServiceCollectionExtensions")]
+		[TestCategory(
+			"AddGravatarHttpClient")]
+		[TestCategory(
+			"Result")]
+		[TestMethod]
+		public void AddGravatarHttpClientTest()
+		{
+			var services =
+				new ServiceCollection();
+
+			services.AddGravatarHttpClient(
+				(options) =>
+				{
+					options.ApiKey =
+						Environment.GetEnvironmentVariable(
+							"GRAVATAR_API_KEY");
+				});
+
+			var serviceProvider =
+				services.BuildServiceProvider();
+
+			var gravatar =
+				serviceProvider.GetService<IGravatarService>();
+
+			Assert.IsNotNull(
+				gravatar);
+		}
+	}
+}
--- source/Gravatar.Tests/GravatarServiceTests.GetProfile.cs
+++ source/Gravatar.Tests/GravatarServiceTests.GetProfile.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Collections;
+using System.Collections.Generic;
 
 using Microsoft;
 using Microsoft.VisualStudio;
@@ -11,6 +13,19 @@ namespace Gravatar
 {
 	partial class GravatarServiceTests
 	{
+		/// <summary>
+		/// 
+		/// </summary>
+		public static IEnumerable<object[]> GetProfileTestData =>
+			[
+				[
+					"05ccee223d9e152175f16538714a9cf8540f4283c009c83c6c9e39c4340ce89d"
+				],
+				[
+					"patchoulish"
+				],
+			];
+
 		/// <summary>
 		/// 
 		/// </summary>
@@ -45,6 +60,39 @@ namespace Gravatar
 			await Assert.ThrowsExceptionAsync<ArgumentException>(
 				() => this.gravatar.GetProfileAsync(
 					String.Empty));
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="hashOrIdentifier"></param>
+		/// <returns></returns>
+		[TestCategory(
+			"GravatarService")]
+		[TestCategory(
+			"GetProfile")]
+		[TestCategory(
+			"Result")]
+		[DataTestMethod]
+		[DynamicData(
+			nameof(GetProfileTestData))]
+		public async Task GetProfileTestAsync(
+			string hashOrIdentifier)
+		{
+			// Sleep for a bit to not stress Gravatar out.
+			Task.Delay(1000)
+				.Wait();
+
+			var profile =
+				await this.gravatar
+					.GetProfileAsync(
+						hashOrIdentifier);
+
+			Assert.IsNotNull(
+				profile);
+
+			Assert.IsNotNull(
+				profile.AvatarUrl);
 		}
 	}
 }
